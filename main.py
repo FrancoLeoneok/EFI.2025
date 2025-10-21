766c2d5ca545ff227ee561bf3ef77f8e8a2dbc6b
FrancoLeoneok <francolopez381@gmail.com>
2025-10-21 19:56:00 -0300

    Add main function and import statements
    Initial implementation of main function with imports and menu structure.

---
+++ main.py
@@ -0,0 +1,12 @@
+main()->None:
+import 
+	// lee el archivo .csv
+import 
+	// gestor_ventas.py
+import
+	// gestor_productos.py
+while menu:
+	// menu de opciones gestion de productos o gestion de     
+	ventas
+if __name__ == ‘__main__’:
+	main()
